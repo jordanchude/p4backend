4817f72e83fc7e085db02bf0b4b790d31308c49c
JordanC <jcwilliams410@gmail.com>
2020-09-15 11:29:12 -0400

    added Link model

--- api/models.py
+++ api/models.py
@@ -4,7 +4,7 @@ from p4backend import settings
 
 
 class Movie(models.Model):
-    image = models.URLField(max_length=200)
+    image = models.URLField()
     title = models.CharField(max_length=40)
     description = models.CharField(max_length=200)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=1)
@@ -17,9 +17,11 @@ class Movie(models.Model):
 
 
 class Link(models.Model):
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, related_name='links', on_delete=models.CASCADE)
     outlet = models.CharField(max_length=20)
-    link = models.URLField(max_length=200)
+    link = models.URLField()
+    is_public = models.BooleanField(default=False)
 
     class Meta:
         verbose_name_plural = 'links'
